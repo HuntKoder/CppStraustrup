a110182c2911c20080fb9b7c6705a82339e8a0bb
HantKoder <huntkoder@gmail.com>
2019-06-01 15:06:53 +0300

    update 01.06.2019 15:06

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int main()
+inline void keep_window_open()
 {
+	char ch;
+	cin >> ch;
+}
 
-	cout << "Hello World" << endl;
+
+int main(	)
+{
+	cout << "Hello World!"		  << endl;
+	cout << "Hello, programming!" << endl;
+	cout << "Here we go!"		  << endl;
+
+	keep_window_open();
 
 	system("pause");
 	return(0);
